ac89ac3aa128df76e5b946e546d0f81c8cf19955
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-13 15:41:16 -0400

    Set up knex schema for users, books, and reviews tables

--- data/migrations/20190413152117_books.js
+++ data/migrations/20190413152117_books.js
@@ -1,8 +1,57 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema
+    .createTable("users", table => {
+      table.increments();
+      table.string("firstName", 30).notNullable();
+      table.string("lastName", 40).notNullable();
+      table
+        .string("username", 30)
+        .notNullable()
+        .unique();
+      table.string("password", 100).notNullable();
+      table
+        .string("thumbnailUrl", 256)
+        .defaultTo("https://pbs.twimg.com/media/C8QsNInXUAAyjZQ.jpg");
+    })
+    .createTable("books", table => {
+      table.increments();
+      table
+        .integer("user_id")
+        .unsigned()
+        .references("users.id")
+        .notNullable()
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      table.string("author", 40).notNullable();
+      table.string("name", 40).notNullable();
+      table.string("publisher", 40).notNullable();
+      table.string("description", 600);
+      table.string("imageUrl", 256);
+    })
+    .createTable("reviews", table => {
+      table.increments();
+      table.string("review", 600).notNullable();
+      table.integer("rating").notNullable();
+      table
+        .integer("book_id")
+        .unsigned()
+        .references("books.id")
+        .notNullable()
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      table
+        .integer("user_id")
+        .unsigned()
+        .references("users.id")
+        .notNullable()
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema
+    .dropTableIfExists("reviews")
+    .dropTableIfExists("books")
+    .dropTableIfExists("users");
 };
