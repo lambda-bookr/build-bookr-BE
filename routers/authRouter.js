a3d7fc3f43bdd75df15becc188ae4ba88b783bcf
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-16 14:20:25 -0400

    Changed casing as necessary in router and helper

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -5,8 +5,8 @@ const db = require("../data/helpers/users-model.js");
 const secret = process.env.SECRET || "It's a secret";
 
 router.post("/register", async (req, res) => {
-  let { username, password, firstname, lastname } = req.body;
-  if (!username || !password || !firstname || !lastname) {
+  let { username, password, firstName, lastName } = req.body;
+  if (!username || !password || !firstName || !lastName) {
     res.status(401).json({ message: "Please enter valid credentials." });
   } else {
     password = bcrypt.hashSync(password, 8);
@@ -14,8 +14,8 @@ router.post("/register", async (req, res) => {
       const newUser = await db.create({
         username,
         password,
-        firstname,
-        lastname
+        first_name: firstName,
+        last_name: lastName
       });
       if (newUser) {
         const token = generateToken(newUser.id, username);
