21ba9711bee46999d1a23eb8a5c37f3cff0acd7a
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-15 21:30:34 -0400

    Attempt number 6

--- data/helpers/books-model.js
+++ data/helpers/books-model.js
@@ -19,39 +19,33 @@ async function find() {
 }
 
 async function findById(id) {
-  let bookContent = db("books")
+  let bookContent = db.raw(
+    `select books.id as id, books.user_id as user_id, users.firstname as firstName, users.lastname as lastName, users.username as username, users.thumbnailurl as thumbnailUrl, books.name as name, books.author as author, books.price as price, books.publisher as publisher, books.imageurl as imageUrl, books.description as description, (select avg(reviews.rating) from reviews where reviews.book_id = ${id}) as rating from books join reviews on reviews.book_id = books.id join users on books.user_id = users.id where books.id = ${id} group by books.id order by books.id`
+  );
+  let bookReviews = db("reviews")
     .select({
-      id: "books.id",
-      user_id: "books.user_id",
-      imageUrl: "books.imageurl",
-      author: "books.author",
-      name: "books.name",
-      price: "books.price",
-      publisher: "books.publisher",
-      description: "books.description",
+      id: "reviews.id",
+      review: "reviews.review",
+      rating: "reviews.rating",
       username: "users.username",
-      firstName: "users.firstName",
-      lastName: "users.lastName",
       thumbnailUrl: "users.thumbnailurl"
     })
-    .innerJoin("users", "books.user_id", "users.id")
-    .where({ "books.id": id })
-    .first();
-  let bookReviews = db.raw(
-    `select reviews.id as id, users.username as username, reviews.review as review, reviews.rating as rating, users.thumbnailurl as thumbnailUrl,
-      (select avg(reviews.rating) from reviews where reviews.book_id = ${id}) as avgRating
-      from reviews
-      join users on reviews.user_id = users.id
-      where reviews.book_id = ${id}`
-  );
+    .innerJoin("users", "reviews.user_id", "users.id")
+    .where({ "reviews.book_id": id });
   const retrieval = await Promise.all([bookContent, bookReviews]);
+  if (process.env.DB_ENVIRONMENT === "production") {
+    if (retrieval[0].rows) {
+      let [content] = retrieval[0].rows;
+      let reviews = retrieval[1];
+      return { ...content, reviews };
+    }
+  }
   if (retrieval[0]) {
     /* This is only true if both the promise resolved AND the post exists. Checking for just the promise causes
     nonexistent posts to return an empty object and array due to my return statement returning an object by default */
-    let content = retrieval[0];
+    let [content] = retrieval[0];
     let reviews = retrieval[1];
-    let rating = reviews[0].avgRating;
-    return { ...content, rating, reviews };
+    return { ...content, reviews };
   }
 }
 
