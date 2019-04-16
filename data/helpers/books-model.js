4a8048a7a798f6918cfe7defd79fe2a53e475b14
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-15 21:08:45 -0400

    Attempt number 4

--- data/helpers/books-model.js
+++ data/helpers/books-model.js
@@ -10,8 +10,11 @@ module.exports = {
 
 async function find() {
   const books = await db.raw(
-    `select books.id as id, books.user_id as user_id, books.name as name, books.author as author, books.price as price, books.publisher as publisher, books.imageurl as imageUrl, books.description as description, (select avg(reviews.rating) from reviews where reviews.book_id = books.id) as rating from books join reviews on reviews.book_id = books.id group by books.id`
+    `select books.id as id, books.user_id as user_id, books.name as name, books.author as author, books.price as price, books.publisher as publisher, books.imageurl as imageUrl, books.description as description, (select avg(reviews.rating) from reviews where reviews.book_id = books.id) as rating from books join reviews on reviews.book_id = books.id group by books.id order by books.id`
   );
+  if (process.env.DB_ENVIRONMENT === "production") {
+    return books.rows;
+  }
   return books;
 }
 
@@ -36,8 +39,7 @@ async function findById(id) {
     .first()
     .innerJoin("reviews", "reviews.book_id", "books.id")
     .avg("rating as rating")
-    .where({ "reviews.book_id": id })
-    .groupBy("books.id");
+    .where({ "reviews.book_id": id });
   let bookReviews = db("reviews")
     .select({
       id: "reviews.id",
