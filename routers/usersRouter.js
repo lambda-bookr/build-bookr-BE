69070405a8d8f73d1697c579808f65f37ace7dbc
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-15 23:43:36 -0400

    Added endpoint fetching user by ID

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,6 +1,22 @@
 const router = require("express").Router();
 const db = require("../data/helpers/users-model.js");
 
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await db.findById(id);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res
+        .status(404)
+        .json({ message: "User with specified ID does not exist." });
+    }
+  } catch (error) {
+    res.status(500).json({ message: `User request failed ${error}.` });
+  }
+});
+
 router.delete("/:id", async (req, res) => {
   const { id } = req.params;
   try {
